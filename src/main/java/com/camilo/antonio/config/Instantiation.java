22c0fdbd4e810add9464655aaec4993e9eb6a41a
Antonio Santos <acamilo10@hotmail.com>
2023-02-14 14:18:55 -0300

    Projection author data with  DTO

--- src/main/java/com/camilo/antonio/config/Instantiation.java
+++ src/main/java/com/camilo/antonio/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.camilo.antonio.config;
 
 import com.camilo.antonio.domain.Post;
 import com.camilo.antonio.domain.User;
+import com.camilo.antonio.dto.AuthorDTO;
 import com.camilo.antonio.repository.PostRepository;
 import com.camilo.antonio.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User user2 = new User(null, "Alex Green", "alex@gmail.com");
         User user3 = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", user1);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", user1);
-
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
-        postRepository.saveAll(Arrays.asList(post1,post2));
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, Abraços!", new AuthorDTO(user1));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(user1));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
