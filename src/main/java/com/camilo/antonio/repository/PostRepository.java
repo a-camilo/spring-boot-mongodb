ea89766aa1383bee6b4de1bb10f963fa9b037b96
Antonio Santos <acamilo10@hotmail.com>
2023-02-15 21:52:02 -0300

    multicriteria query

--- src/main/java/com/camilo/antonio/repository/PostRepository.java
+++ src/main/java/com/camilo/antonio/repository/PostRepository.java
@@ -4,6 +4,7 @@ import com.camilo.antonio.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.Date;
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
@@ -12,4 +13,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     List<Post> searchByTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/com/camilo/antonio/services/PostService.java
+++ src/main/java/com/camilo/antonio/services/PostService.java
@@ -6,6 +6,7 @@ import com.camilo.antonio.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,13 +15,18 @@ public class PostService {
     @Autowired
     private PostRepository repository;
 
-    public Post findById(String id){
+    public Post findById(String id) {
         Post post = repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
         return post;
     }
 
-    public List<Post> findByTitle(String text){
+    public List<Post> findByTitle(String text) {
         return repository.searchByTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 
 }
